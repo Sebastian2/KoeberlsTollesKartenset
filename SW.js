712584ff8bf50d1b0733ab826f0fc3af02360c0a
Sebastian2 <sebastiankoeberl23@gmail.com>
2023-03-13 21:17:40 +0100

    Hello 1

---
+++ SW.js
@@ -0,0 +1,26 @@
+
+let currentPath = self.location.href.replace("/sw.js", "");
+
+const cacheName = "Danke_Ewald"
+const assets = [
+    "/",
+    "/index.html",
+    "/script.js"
+   
+].map(url => currentPath + url)
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
